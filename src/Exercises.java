9f5ace725032916ee2860380eed6a4546c49bc49
Michael Ryan <mike.ryan871@gmail.com>
2020-01-20 21:40:41 -0500

    Exercise 2 (endsMeet) Completed.

--- src/Exercises.java
+++ src/Exercises.java
@@ -14,49 +14,66 @@ public class Exercises {
 	}
 
 	public String[] endsMeet(String[] values, int n) {
-		if (values == null || values.length < n|| n < 0) {
-			return null;
+
+		String empty[] = new String[0];
+
+		if (values == null || values.length < n || n < 0) {
+			return empty;
 		}
 		// write your code here
 
-		return null;	// default return value to ensure compilation
-	}
+		String result[] = new String[n * 2];
+		int length = values.length;
+		int index = n;
 
-	/*public int difference(int[] numbers) {
-		if (numbers == null || numbers < 1) {
+		for (int i = 0; i < n; i++) {
+			result [i] = values[i];
+		}
+
+		for (int x = n; x < 2 * n; x++) {
+				result[x] = values[length - index];
+				index--;
+		}
+
+		return result;
+
+		// return null;	// default return value to ensure compilation
+	}
+/*
+	public int difference(int[] numbers) {
+		if (numbers == null || numbers.length < 1) {
 			return -1;
 		}
 		// write your code here
 
 		return -1;		// default return value to ensure compilation
 	}
-*/
+
 	public double biggest(double[] numbers) {
 		if (numbers == null || numbers.length < 3 || numbers.length % 2 == 0){
 			return -1;
 		}
 		// write your code here
 		double max = numbers[0];
-		if (max <= numbers[numbers.length-1])
-				max = numbers[numbers.length-1];
-		if(max <= numbers[numbers.length / 2])
+		if (max <= numbers[numbers.length - 1])
+				max = numbers[numbers.length - 1];
+		if (max <= numbers[numbers.length / 2])
 				max = numbers[numbers.length / 2];
 				return max;
 
 			// return -1;		// default return value to ensure compilation
 		}
 
-/*
+
 	public String[] middle(String[] values) {
-		if (values == null || values < 3 || values.length % 2 == 0){
-			return null;
-		}
-		// write your code here
+		if (values == null || values.length < 3 || values.length % 2 == 0) {
+			return empty;
 
 
-		return null;	// default return value to ensure compilation
+
+		// return null;	// default return value to ensure compilation
 	}
-
+/*
 	public boolean increasing(int[] numbers) {
 		if (numbers == null || numbers < 3){
 			return false;

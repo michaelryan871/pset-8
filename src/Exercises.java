6c61fe668f5274c4f8adc2ce6cc5bd9828378063
Michael Ryan <mike.ryan871@gmail.com>
2020-01-20 20:37:27 -0500

    Exercise 4 (biggest) Completed.

--- src/Exercises.java
+++ src/Exercises.java
@@ -9,42 +9,50 @@ public class Exercises {
 		if  (a[0] == b[0] || a[a.length-1] == b[b.length-1]) {
 			return true;
 		} else {
-			return false;	// default return value to ensure compilation	
+			return false;	// default return value to ensure compilation
 		}
 	}
 
 	public String[] endsMeet(String[] values, int n) {
 		if (values == null || values.length < n|| n < 0) {
-			return false; 
+			return null;
 		}
 		// write your code here
 
 		return null;	// default return value to ensure compilation
 	}
 
-	public int difference(int[] numbers) {
+	/*public int difference(int[] numbers) {
 		if (numbers == null || numbers < 1) {
-			return false; 
+			return -1;
 		}
 		// write your code here
 
 		return -1;		// default return value to ensure compilation
 	}
-
+*/
 	public double biggest(double[] numbers) {
-		if (numbers == null || numbers < 3 || numbers.length % 2 == 0){
-			return -1; 
+		if (numbers == null || numbers.length < 3 || numbers.length % 2 == 0){
+			return -1;
 		}
 		// write your code here
+		double max = numbers[0];
+		if (max <= numbers[numbers.length-1])
+				max = numbers[numbers.length-1];
+		if(max <= numbers[numbers.length / 2])
+				max = numbers[numbers.length / 2];
+				return max;
 
-		return -1;		// default return value to ensure compilation
-	}
+			// return -1;		// default return value to ensure compilation
+		}
 
+/*
 	public String[] middle(String[] values) {
-		if (numbers == null || numbers < 3 || numbers.length % 2 == 0){
-			return null; 
+		if (values == null || values < 3 || values.length % 2 == 0){
+			return null;
 		}
 		// write your code here
+
 
 		return null;	// default return value to ensure compilation
 	}
@@ -60,7 +68,7 @@ public class Exercises {
 
 	public boolean everywhere(int[] numbers, int x) {
 		if (values == null || values < 1){
-			return false; 
+			return false;
 		}
 		// write your code here
 
@@ -75,7 +83,7 @@ public class Exercises {
 
 		return false;	// default return value to ensure compilation
 	}
-														
+
 	public boolean balance(int[] numbers) {
 		if (numbers == null || numbers < 2){
 			return false;
@@ -89,20 +97,21 @@ public class Exercises {
 		if (values == null){
 			return -1;
 		}
-		
+
 		boolean containsNull = false;
 		for (int i = 0; i < values.length; i++){
 			if (values[i] == null){
 				containsNull = true;
 			}
 		}
-		
+
 		if (containsNull) {
-			return -1; 
+			return -1;
 		}
-		
+
 		// write your code here
 
 		return -1;		// default return value to ensure compilation
 	}
+	*/
 }
